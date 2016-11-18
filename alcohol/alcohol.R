580ce397c7df500f4b8f2086f7aa63eb751496f8
Shirin Glander <s_glan02@wwu.de>
2016-11-18 16:26:42 +0100

    Worked on GG 2

---
+++ alcohol/alcohol.R
@@ -0,0 +1,17 @@
+install.packages("ttbbeer")
+
+# https://mran.microsoft.com/web/packages/ttbbeer/ttbbeer.pdf
+
+library(ttbbeer)
+
+head(beermaterials)
+
+
+head(artificialcarbontax)
+head(beertax)
+head(champagnetax)
+head(spirittax)
+head(tobaccotax)
+head(winetax14)
+head(winetax1421)
+head(winetax2124)
