4f9988067767270a5f71345ce3da57b0a1478aa1
Shirin Glander <s_glan02@wwu.de>
2017-01-31 17:02:41 +0100

    Updated timeline

--- timeline/app.R
+++ timeline/app.R
@@ -1,23 +1,33 @@
 # https://github.com/daattali/timevis
 
 library(shiny)
+library(shinythemes )
 library(timevis)
 
 data <- data.frame(
   content = c("Chip-Seq Marie Liebmann",
               "Paper schreiben",
               "Antrag übersetzen & überarbeiten",
-              "Vortrag für Informatikseminar vorbereiten auf Englisch!",
+              "Vortrag für Informatikseminar vorbereiten<br>(auf Englisch!)",
               "Informatikseminar",
               "Protein-miRNA-WGCNA",
               "Doktorarbeit veröffentlichen ML to predict df based on ft"),
   start = c("2017-02-01", "2017-02-01", "2017-02-01", "2017-02-01", "2017-02-23 14:00:00", "2017-04-01", "2017-04-01"),
-  end = c("2017-02-03", "2017-02-24", "2017-02-24", "2017-02-23", NA, NA, NA)
+  end = c("2017-02-03", "2017-02-24", "2017-02-24", "2017-02-23", NA, NA, NA),
+  type = c("background", "range", "range", "range", "point", "box", "box"),
+  group = c(1, 2, 2, 3, 3, 2, 2),
+  style = c("color: grey;", NA, NA, "color: red;", NA, NA, NA)
 )
 
 data$id <- seq_len(nrow(data))
 
-ui <- fluidPage(
+groups <- data.frame(
+  id = 1:3,
+  content = c("Collaboration projects", "My own projects", "Presentations/ Seminars")
+)
+
+
+ui <- fluidPage(theme = shinytheme("flatly"),
   titlePanel("To do timeline"),
 
   timevisOutput("timeline")
@@ -25,7 +35,7 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$timeline <- renderTimevis({
-    timevis(data)
+    timevis(data, groups, fit = FALSE, options = list(editable = TRUE, multiselect = TRUE, align = "left"))
   })
 }
 
